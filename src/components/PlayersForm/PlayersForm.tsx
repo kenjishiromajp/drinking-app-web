9341be38a0375b733b208840b9142e05578a64e9
kenjishiromajp <kenjishiromajp@gmail.com>
2022-07-10 16:45:00 +1000

    feat(players-form): players form

---
+++ src/components/PlayersForm/PlayersForm.tsx
@@ -0,0 +1,73 @@
+import {
+  Button,
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  HStack,
+  Box,
+} from '@chakra-ui/react';
+import { useForm, useFieldArray } from 'react-hook-form';
+import { FormHTMLAttributes } from 'react';
+
+export interface Player {
+  name: string;
+}
+export interface PlayersFormValues {
+  players: Player[];
+}
+export interface PlayersFormProps
+  extends Omit<FormHTMLAttributes<HTMLFormElement>, 'onSubmit'> {
+  onSubmit: (data: PlayersFormValues) => any;
+}
+export default function PlayersForm({
+  onSubmit = () => {},
+  ...props
+}: PlayersFormProps) {
+  const { register, handleSubmit, control } = useForm<PlayersFormValues>({
+    defaultValues: { players: [{ name: '' }] },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control, // control props comes from useForm (optional: if you are using FormContext)
+    name: 'players', // unique name for your Field Array
+  });
+
+  return (
+    <form {...props} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        {fields.map((field, index) => (
+          <FormControl key={field.id}>
+            <HStack alignItems="end">
+              <Box flex="1">
+                <FormLabel>Player {index + 1}</FormLabel>
+                <Input {...register(`players.${index}.name`)} />
+              </Box>
+              <Button onClick={() => remove(index)}>Remove</Button>
+            </HStack>
+          </FormControl>
+        ))}
+        <Button
+          // bg="blue.400"
+          // color="white"
+          // _hover={{
+          //   bg: 'blue.500',
+          // }}
+          onClick={() => append({ name: '' })}
+        >
+          Add player
+        </Button>
+        <Button
+          type="submit"
+          bg="blue.400"
+          color="white"
+          _hover={{
+            bg: 'blue.500',
+          }}
+        >
+          Save
+        </Button>
+      </Stack>
+    </form>
+  );
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,17 @@
+import PlayersForm, {
+  PlayersFormValues,
+} from '../../components/PlayersForm/PlayersForm';
 import MainLayout from '../../layouts/MainLayout/MainLayout';
 
 function HomePage() {
   return (
     <MainLayout>
-      <h1>HomePage</h1>
+      <h1>Register your players</h1>
+      <PlayersForm
+        onSubmit={(data: PlayersFormValues) => {
+          console.log('data', data);
+        }}
+      />
     </MainLayout>
   );
 }
